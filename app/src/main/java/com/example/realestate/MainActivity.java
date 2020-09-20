a8abbd4d6380ed20f2516546f7e161978757e224
vinay yadav <vinayadav99@gmail.com>
2020-09-20 21:54:26 +0530

    Adding all the files to the commit

---
+++ app/src/main/java/com/example/realestate/MainActivity.java
@@ -0,0 +1,34 @@
+package com.example.realestate;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.graphics.drawable.AnimationDrawable;
+import android.os.Bundle;
+import android.os.Handler;
+import android.widget.ImageView;
+
+public class MainActivity extends AppCompatActivity {
+
+    private ImageView loadImage;
+    private AnimationDrawable loadAnimation;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        loadImage = findViewById(R.id.loadID);
+        loadImage.setBackgroundResource(R.drawable.load_animation);
+        loadAnimation = (AnimationDrawable)loadImage.getBackground();
+        loadAnimation.start();
+        Handler mHandler = new Handler();
+        mHandler.postDelayed(new Runnable() {
+            @Override
+            public void run() {
+                loadAnimation.stop();
+                MainActivity.this.finish();
+            }
+        },3000);
+
+    }
+}
